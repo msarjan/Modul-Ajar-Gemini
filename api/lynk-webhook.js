4d38f8209b23c3d2417983eae346e17b44f78825
msarjan <sarjanalika@gmail.com>
2026-06-10 22:14:41 +0800

    Update lynk-webhook.js

--- api/lynk-webhook.js
+++ api/lynk-webhook.js
@@ -15,7 +15,7 @@ const PRODUK_KUOTA = {
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // Generate kode GBD-XXXXXX (6 karakter, tanpa 0/O dan 1/I)
